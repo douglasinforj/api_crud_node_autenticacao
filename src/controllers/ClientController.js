0d06490b1076774c0752574f01b58311fcd9c0eb
Douglas Silva <douglasitpro@gmail.com>
2025-04-14 23:35:23 -0300

    Arquivo trata das requisições na rotas

---
+++ src/controllers/ClientController.js
@@ -0,0 +1,44 @@
+import Client from '../models/Client.js';
+import * as ClientService from '../services/ClientService.js';
+
+export const create = async (req, res) => {
+    try {
+      const data = req.body;
+      if (req.file) data.foto = req.file.filename;
+      const client = await ClientService.createClient(data);
+      res.status(201).json(client);
+    } catch (err) {
+      res.status(400).json({ message: err.message });
+    }
+};
+
+export const list = async (req, res) => {
+    const clients = await ClientService.getClients();
+    res.json(clients);
+};
+
+export const show = async (req, res) => {
+    const client = await ClientService.getClientById(req.params.id);
+    if (!client) return res.status(404).json({ message: 'Não encontrado' });
+    res.json(client);
+};
+
+export const update = async (req, res) => {
+    try {
+      const data = req.body;
+      if (req.file) data.foto = req.file.filename;
+      const client = await ClientService.updateClient(req.params.id, data);
+      res.json(client);
+    } catch (err) {
+      res.status(400).json({ message: err.message });
+    }
+};
+
+export const remove = async (req, res) => {
+    try {
+      await ClientService.deleteClient(req.params.id);
+      res.json({ message: 'Cliente excluído' });
+    } catch (err) {
+      res.status(400).json({ message: err.message });
+    }
+  };
